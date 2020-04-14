626751b5e17fd6454d1b73d12c630a872bac1cc0
Dylan Smith <dylan1999.smith@gmail.com>
2020-04-13 21:39:11 -0500

    Added animations to header:

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ const Header = () => {
     return ( 
     <div className="Header">
         <h1>Hey, <br/> I'm Dylan Smith.</h1>
+        <h2>Software Engineering Major at <br/> Iowa State University</h2>
     </div> );
 }
  
