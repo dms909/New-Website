091825c636682dbad0c7319a0ec1ad668fb66a28
Dylan Smith <dylan1999.smith@gmail.com>
2020-04-14 16:10:51 -0500

    added aboutme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
+import AboutMe from './components/AboutMe';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AboutMe />
     </div>
   );
 }
