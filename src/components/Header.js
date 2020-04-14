994de8447bbbe4a603d36cc0842551ff27b86845
Dylan Smith <dylan1999.smith@gmail.com>
2020-04-13 21:21:57 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return ( 
+    <div className="Header">
+        <h1>Hey, <br/> I'm Dylan Smith.</h1>
+    </div> );
+}
+ 
+export default Header;
