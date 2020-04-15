56852e621138cb9ea71c35a5cdafd95682d7c540
Dylan Smith <dylan1999.smith@gmail.com>
2020-04-15 12:34:46 -0500

    Added education section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import AboutMe from './components/AboutMe';
+import Education from './components/Education';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <AboutMe />
+      <Education />
     </div>
   );
 }
--- src/components/Education.js
+++ src/components/Education.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Education = () => {
-    return (  );
+    return ( 
+        <div className="Education">
+            <h2>Education</h2>
+            <hr align="left"/>
+
+            <h4>Fall 2017 - Present</h4>
+            <h3>Iowa State University</h3>
+            <h4>Bachelors Degree</h4>
+
+        </div>
+     );
 }
  
 export default Education;
