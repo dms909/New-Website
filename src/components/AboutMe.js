9db4095034ae58e515be9251663a3b0edde10941
Dylan Smith <dylan1999.smith@gmail.com>
2020-04-15 12:03:53 -0500

    Finished about me text

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -12,12 +12,20 @@ const AboutMe = () => {
                         Currently, I am a junior at Iowa State University as a software engineering major. This upcoming summer, I'll be a software engineer 
                         intern for the individual disability team at Principal Financial Group. Previously, I was an intern at Hy-Vee's technology center, HST.
                         Over the past year, I've also been working as a Computer Science 227 Teaching Assistant. From my experience within the software field,
-                        I've learned  
+                        I've learned a lot about the discipline, and what creates good software. As I move forward in my career, I hope to continue to learn and
+                        create products that I love.
                     </p>
                 </div>
 
                 <div className="FunAbout">
-                    <p> On top of software development, I am very passioniate about music.</p>
+                    <p> Although most of my experience comes from professional enviroments, I enjoy learning new technologies on my own time. Currently, I am working
+                        on an application to assist employees who work from home. Some of my favorite technologies I've been learning lately have been React, Python,
+                        C#, and SpringBoot.
+                        <br />
+                        <br />
+                        On top of software development, I am very passioniate about music. In my time at Iowa State, I played in a band and was a member of the GENRE
+                        music club. I play guitar, and I'm currently learning the piano. Check out the music tab to see what I've been listening to on Spotify!
+                    </p>
                 </div>
             </div>
         </div>
