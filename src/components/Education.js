e82b9b0224eac9e62df0ba782cc62f23af8fbf55
Dylan Smith <dylan1999.smith@gmail.com>
2020-04-19 23:04:22 -0500

    Updated education section

--- src/components/Education.js
+++ src/components/Education.js
@@ -8,6 +8,7 @@ const Education = () => {
 
             <h4>Fall 2017 - Present</h4>
             <h3>Iowa State University</h3>
+            <h4>Ames, Iowa</h4>
             <h4>Bachelors Degree</h4>
             <p>Majoring in Software Engineering</p>
             <p>Minoring in Cyber Security</p>
