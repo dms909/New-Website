49fe6ea48b51dd92c742432e06063b2622018423
Dylan Smith <dylan1999.smith@gmail.com>
2020-04-27 20:13:09 -0500

    Fixed education header

--- src/components/Education.js
+++ src/components/Education.js
@@ -6,8 +6,8 @@ const Education = () => {
             <h2>Education</h2>
             <hr align="left"/>
 
-            <h3 className="EducationHeader">Iowa State University</h3>
-            <h4 className="EducationDate">Fall 2017 - Present</h4>
+            <h3 className="EducationSchoolName">Iowa State University</h3>
+            <h4 className="EducationDate"><i>Fall 2017 - Present</i></h4>
     
             <h4>Bachelors of Science in Software Engineering</h4>
             <h4>Minor in Cyber Security</h4>
