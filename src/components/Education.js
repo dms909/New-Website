65ac15943d917082ce46e329b500a59bed584a61
Dylan Smith <dylan1999.smith@gmail.com>
2020-05-12 14:08:11 -0500

    fixed education p tags

--- src/components/Education.js
+++ src/components/Education.js
@@ -9,8 +9,12 @@ const Education = () => {
             <h3 className="EducationSchoolName">Iowa State University</h3>
             <h4 className="EducationDate"><i>Fall 2017 - Present</i></h4>
     
-            <h4>Bachelors of Science in Software Engineering</h4>
-            <h4>Minor in Cyber Security</h4>
+            <h4 className="EducationMajor">Bachelors of Science in Software Engineering</h4>
+            <h4 className="EducationMinor">Minor in Cyber Security</h4>
+
+            <p className="EducationCoursework"><b>Relevant Coursework:</b> Computer Science 352 - Operating Systems, Computer Science 311 - Algorithms, Computer Science 363 - 
+            Databases, Software Engineering 329 - Project Management,  Computer Science 228 - Data Structures, Computer Science 327 - Advanced Programming Concepts,
+            Computer Science 227 - Object Oriented Programming</p>
 
         </div>
      );
