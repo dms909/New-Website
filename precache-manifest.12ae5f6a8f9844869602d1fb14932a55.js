9e8a3899aed571418859ae411d9740ac7e17bed2
Dylan Smith <dylan1999.smith@gmail.com>
2020-07-21 18:24:22 -0500

    Updates

---
+++ precache-manifest.12ae5f6a8f9844869602d1fb14932a55.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5787a054445697a3b7da74d8f3f6b6a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1467a32ff8b179d894d0",
+    "url": "/static/css/2.9a9b1a2c.chunk.css"
+  },
+  {
+    "revision": "defc6bffcdc9f878760c",
+    "url": "/static/css/main.e6cca4ce.chunk.css"
+  },
+  {
+    "revision": "1467a32ff8b179d894d0",
+    "url": "/static/js/2.0ed2ce1e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0ed2ce1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "defc6bffcdc9f878760c",
+    "url": "/static/js/main.210418af.chunk.js"
+  },
+  {
+    "revision": "83d861f9b783c604af74",
+    "url": "/static/js/runtime-main.068ff63e.js"
+  },
+  {
+    "revision": "eeaf0bc0be9b2a702d2e5643e4061c42",
+    "url": "/static/media/exercyze.eeaf0bc0.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0bef61f5470ac97f1f149aff538e3eb1",
+    "url": "/static/media/icons8-spring-logo.0bef61f5.svg"
+  },
+  {
+    "revision": "b31406d53786e9b4a433b436fac958f6",
+    "url": "/static/media/me.b31406d5.jpg"
+  },
+  {
+    "revision": "3e73bbd4d3f15dbbe22bb57d3abc6bbb",
+    "url": "/static/media/twitter_logo_volt.3e73bbd4.png"
+  }
+]);
